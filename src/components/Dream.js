3a018309f09d4e54b001d1fa3aba2a6894ed63ea
Misty DeRamus <mistyderamus@gmail.com>
2019-07-30 10:59:50 -0500

    added sign in page

--- src/components/Dream.js
+++ src/components/Dream.js
@@ -3,12 +3,14 @@ import NavBar from "./nav/NavBar";
 import ApplicationViews from "../ApplicationViews";
 import "bootstrap/dist/css/bootstrap.min.css"
 import "./Dream.css";
+import Login from "./authentication/Login";
 
 export default class Dream extends Component {
   render() {
     return (
       <React.Fragment>
         <NavBar />
+        <Login />
         {/* <ApplicationViews /> */}
       </React.Fragment>
     );
