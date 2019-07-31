07b6a64159df8aef61b6365d1542d95a7ab08061
Misty DeRamus <mistyderamus@gmail.com>
2019-07-31 15:35:26 -0500

    working on fetching goals

--- src/ApplicationViews.js
+++ src/ApplicationViews.js
@@ -6,6 +6,7 @@ import JournalList from "./components/journal/JournalList";
 import APIManager from "./components/modules/APIManager";
 import Login from "./components/authentication/Login";
 import Register from "./components/authentication/Register";
+import GoalForm from "./components/goal/GoalForm";
 
 export default class ApplicationViews extends Component {
   // Check if credentials are in local storage
@@ -13,25 +14,46 @@ export default class ApplicationViews extends Component {
 
   state = {
     goals: [],
+    rewards: [],
     journals: []
   };
 
-  getUserGoals = () => {
-    APIManager.getAll(sessionStorage.getItem("userId")).then(user_goals =>
-      this.setState({ goals: user_goals })
-    );
-  };
+  componentDidMount() {
+    const newState = {};
+  }
+    getUserGoals = () => {
+      APIManager.getAll(sessionStorage.getItem("userId")).then(user_goals =>
+        this.setState({ goals: user_goals })
+      );
+    };
 
-  deleteGoal = id => {
-    return fetch(`http://localhost:5002/goals/${id}`, {
-      method: "DELETE"
-    })
-      .then(APIManager.getAll)
-      .then(goals => {
-        this.props.history.push("/goals");
-        this.setState({ goals: goals });
-      });
-  };
+    deleteGoal = id => {
+      return fetch(`http://localhost:5002/goals/${id}`, {
+        method: "DELETE"
+      })
+        .then(APIManager.getAll)
+        .then(goals => {
+          this.props.history.push("/goals");
+          this.setState({ goals: goals });
+        });
+    };
+    addGoal = goal =>
+      APIManager.post(goal)
+        .then(() => APIManager.getAll())
+        .then(goals =>
+          this.setState({
+            goals: goals
+          })
+        );
+    addReward = reward =>
+      APIManager.getAll(reward)
+        .then(() => APIManager.getAll())
+        .then(rewards =>
+          this.setState({
+            rewards: rewards
+          })
+        );
+
   render() {
     return (
       <React.Fragment>
@@ -57,6 +79,19 @@ export default class ApplicationViews extends Component {
               <GoalList
                 goals={this.state.goals}
                 deleteGoal={this.state.deleteGoal}
+              />
+            );
+          }}
+        />
+        <Route
+          exact
+          path="/goals/new"
+          render={props => {
+            return (
+              <GoalForm
+                {...props}
+                addGoal={this.addGoal}
+                rewards={this.state.rewards}
               />
             );
           }}
